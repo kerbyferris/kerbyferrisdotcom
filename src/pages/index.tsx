368a0204b88e24231fee9a9ceb05538f6eab6c3c
Kerby Ferris <kerbyferri@gmail.com>
2023-05-31 23:58:59 +0200

    factor out navigation into component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Link from "next/link";
 import { Inter } from "next/font/google";
 import { type NextPage } from "next";
 import Head from "next/head";
 import { Media, Categories } from "~/lib/data";
+import Navigation from "~/components/navigation";
 import GridItem from "~/components/grid-item";
 import Footer from "~/components/footer";
 import { useState } from "react";
@@ -21,9 +21,6 @@ const Home: NextPage = () => {
     currentCategory === Categories.None
       ? Media.filter((media) => media.homePage)
       : Media.filter((media) => media.categories.includes(currentCategory));
-  const categories = Object.values(Categories).filter(
-    (category) => category !== Categories.None
-  );
 
   return (
     <>
@@ -35,40 +32,10 @@ const Home: NextPage = () => {
 
       <main className={`${text.className} grid min-w-full place-items-center`}>
         <div className="max-w-screen-2xl">
-          <nav className="min-w-full bg-header_bg_image bg-cover pl-8 font-bold text-white">
-            <h1
-              className="cursor-pointer py-5 text-8xl uppercase leading-[72px] hover:text-yellow-300 lg:text-9xl"
-              onClick={() => setCurrentCategory(Categories.None)}
-            >
-              Kerby Ferris
-            </h1>
-            <ul className="px-[-2px] py-5 text-7xl leading-[53px] lg:text-8xl lg:leading-[72px]">
-              {categories.map((category) => (
-                <li key={category}>
-                  <Link
-                    className={
-                      category === currentCategory
-                        ? "text-gray-300"
-                        : "hover:text-yellow-300"
-                    }
-                    onClick={() => setCurrentCategory(category)}
-                    href="/"
-                  >
-                    <span
-                      className={
-                        category === currentCategory
-                          ? "text-yellow-300"
-                          : "hidden"
-                      }
-                    >
-                      &#9656;
-                    </span>
-                    {category}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+          <Navigation
+            currentCategory={currentCategory}
+            handler={setCurrentCategory}
+          />
           <div className="justify-items-center md:grid md:grid-flow-row md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {media.map(({ src, poster, loop, externalLink }) => (
               <div
