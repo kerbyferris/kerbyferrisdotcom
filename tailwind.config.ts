2217813cc94c9258ab7144a4ea3789e841a6cc58
Kerby Ferris <kerbyferri@gmail.com>
2024-01-10 20:05:39 +0000

    test: Dropbox hosted video

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,9 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        header_bg_image: "url('/img/showshop.gif')",
+        // header_bg_image: "url('/img/showshop.gif')",
+        header_bg_image:
+          "url('https://www.dropbox.com/scl/fi/ooa0x727wtx5nte4vk0hy/showshop.gif?rlkey=y9qp9zgst43b2unta13xq8cxr&dl=0&raw=1')",
       },
     },
   },
